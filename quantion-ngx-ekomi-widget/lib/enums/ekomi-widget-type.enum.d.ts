f8c2eb2ba244fde029f756642675a492f48056b0
eaymerich10 <80621659+eaymerich10@users.noreply.github.com>
2024-04-25 10:13:50 +0200

    Delete quantion-ngx-ekomi-widget directory

--- quantion-ngx-ekomi-widget/lib/enums/ekomi-widget-type.enum.d.ts
+++
@@ -1,4 +0,0 @@
-export declare enum EkomiWidgetTypeEnum {
-    reputami_rank = "reputami_rank",
-    reputami_showroom = "reputami_showroom"
-}
--- quantion-ngx-ekomi-widget/public-api.d.ts
+++
@@ -1,4 +0,0 @@
-export * from './lib/components/ekomi-rank-widget/ekomi-rank-widget.component';
-export * from './lib/components/ekomi-showroom-widget/ekomi-showroom-widget.component';
-export * from './lib/components/ekomi-smart-widget/ekomi-smart-widget.component';
-export * from './lib/quantion-ngx-ekomi-widget.module';
